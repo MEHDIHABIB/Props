cdc3b645fa66d8d3aab3600bee5d77788b75c9d6
MEHDIHABIB <78062259+MEHDIHABIB@users.noreply.github.com>
2021-03-12 16:27:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import Profile from './profile/Profile'
+import image from './profile/image.jpg'
+import PropTypes from "prop-types"
 
 function App() {
   const handleName = (name) => alert(name) ;
   return (
     <div className="App">
 
-      <Profile fullName="Mehdi Habib" bio="I'm a Tunisian man" profession="I'm a fullstack javaSript student" handleName={handleName} > https://st2.depositphotos.com/1531183/5770/v/950/depositphotos_57709697-stock-illustration-male-person-silhouette-profile-picture.jpg </Profile>
+      
+      <Profile fullName="Mehdi Habib" bio="I'm a Tunisian man" profession="I'm a fullstack javaScript student" handleName={handleName} > <img style={{width:"300px", height:"300px"}} src={image} alt="myImage" /></Profile>
       
     </div>
   );
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Profile = props => {
     return (
         <div>
-            <span> <img style={{width:"200px", height:"200px"}} src="{props.children}" alt="myImage" /> </span>
-            <h1 style={{fontStyle:"italic"}}> {props.fullName} </h1>
+             {props.children}
+            <h1> {props.fullName} </h1>
             <h2> Bio: <span style={{color:"magenta"}}> {props.bio} </span></h2>
             <h3> Profession: <span style={{color:"blue"}}>{props.profession}</span> </h3>
             <button onClick={() => props.handleName("Mehdi Habib")}>Click Here</button>
@@ -14,4 +14,17 @@ const Profile = props => {
     )
 }
 
+Profile.defaultProps={
+    fullName:"Mehdi" ,
+    bio:"I'm Tunisian" ,
+    profession:"Student"
+}
+
+
+
+
+
+
+
 export default Profile
+
